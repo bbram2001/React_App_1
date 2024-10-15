504545b84edffccfb0053492006c9c46e884c25c
bbram2001 <rambharath997@gmail.com>
2024-10-15 16:20:19 +0530

    React File

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./demo/Message";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <Message />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demo/Message.tsx
@@ -0,0 +1,15 @@
+var Message = () => {
+  var num1 = 10;
+
+  var num2 = 20;
+
+  var Total = num1 + num2;
+
+  return (
+    <div>
+      <h1>Total value:{Total}</h1>
+    </div>
+  );
+};
+
+export default Message;
